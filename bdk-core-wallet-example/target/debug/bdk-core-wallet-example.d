06e81458e87788615916afc6f7340b88ec583a86
mecastor <mecastor87@gmail.com>
2023-08-02 16:28:02 +0100

    Created binary package using cargo

---
+++ bdk-core-wallet-example/target/debug/bdk-core-wallet-example.d
@@ -0,0 +1,1 @@
+/Users/mecastor/Desktop/repos/bdk-core-rpc-demo/bdk-core-wallet-example/target/debug/bdk-core-wallet-example: /Users/mecastor/Desktop/repos/bdk-core-rpc-demo/bdk-core-wallet-example/src/main.rs
---
+++ bdk-core-wallet-example/target/debug/deps/bdk_core_wallet_example-a5c6c118c1982ad2.d
@@ -0,0 +1,5 @@
+/Users/mecastor/Desktop/repos/bdk-core-rpc-demo/bdk-core-wallet-example/target/debug/deps/bdk_core_wallet_example-a5c6c118c1982ad2: src/main.rs
+
+/Users/mecastor/Desktop/repos/bdk-core-rpc-demo/bdk-core-wallet-example/target/debug/deps/bdk_core_wallet_example-a5c6c118c1982ad2.d: src/main.rs
+
+src/main.rs:
